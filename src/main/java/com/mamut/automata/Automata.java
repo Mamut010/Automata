95f37445422d6ff386c2c1b63034f601f48697d0
Mamut010 <phucthinh0013@gmail.com>
2024-07-27 12:58:14 +0800

    Initial commit

---
+++ src/main/java/com/mamut/automata/Automata.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mamut.automata;
+
+/**
+ *
+ * @author Pc
+ */
+public class Automata {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

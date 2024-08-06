106ae01b1dfe502e6ac4fce2381b3f89058de847
Mamut010 <phucthinh0013@gmail.com>
2024-08-06 11:56:56 +0800

    Update Movement factory to contain WeakReference of objects for efficient memory usage

--- src/main/java/com/mamut/automata/turing/Movements.java
+++ src/main/java/com/mamut/automata/turing/Movements.java
@@ -6,30 +6,43 @@ package com.mamut.automata.turing;
 
 import com.mamut.automata.turing.movements.*;
 import com.mamut.automata.util.CollectionUtils;
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  *
  * @author Pc
  */
 public final class Movements {
-    private static final Map<String, Movement> cache = new HashMap<>();
+    private static final Map<String, WeakReference<Movement>> cache = new HashMap<>();
     
     private Movements() {}
     
     public static Movement left() {
         final String LEFT_KEY = "L";
-        return CollectionUtils.getOrPutNew(cache, LEFT_KEY, LeftMovement::new);
+        return getOrCacheNew(LEFT_KEY, LeftMovement::new);
     }    
     
     public static Movement right() {
         final String RIGHT_KEY = "R";
-        return CollectionUtils.getOrPutNew(cache, RIGHT_KEY, RightMovement::new);
+        return getOrCacheNew(RIGHT_KEY, RightMovement::new);
     }
     
     public static Movement stay() {
         final String STAY_KEY = "S";
-        return CollectionUtils.getOrPutNew(cache, STAY_KEY, StayMovement::new);
+        return getOrCacheNew(STAY_KEY, StayMovement::new);
+    }
+    
+    private static Movement getOrCacheNew(String key, Supplier<Movement> creator) {
+        WeakReference<Movement> reference = cache.get(key);
+        Movement obj;
+        if (reference == null || reference.get() == null) {
+            obj = creator.get();
+            reference = new WeakReference(obj);
+            cache.put(key, reference);
+        }
+        return reference.get();
     }
 }
